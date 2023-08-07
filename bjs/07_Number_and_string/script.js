532743eef98ab15a98c8d4219203ebe8eef8882c
lozovoy-evgeny <lozovoy.evgeny@yandex.ru>
2023-08-07 15:59:01 +0300

    task 8.7

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -4,6 +4,103 @@ let operation = null;
 const inputWindow = document.getElementById('inputWindow');
 
 
+
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+
+document.getElementById('btn_2').addEventListener('click', function () {
+    inputWindow.value += '2';
+})
+
+document.getElementById('btn_3').addEventListener('click', function () {
+    inputWindow.value += '3';
+})
+
+document.getElementById('btn_4').addEventListener('click', function () {
+    inputWindow.value += '4';
+})
+
+document.getElementById('btn_5').addEventListener('click', function () {
+    inputWindow.value += '5';
+
+})
+
+document.getElementById('btn_6').addEventListener('click', function () {
+    inputWindow.value += '6';
+})
+
+document.getElementById('btn_7').addEventListener('click', function () {
+    inputWindow.value += '7';
+})
+
+document.getElementById('btn_8').addEventListener('click', function () {
+    inputWindow.value += '8';
+})
+
+document.getElementById('btn_9').addEventListener('click', function () {
+    inputWindow.value += '9';
+})
+
+document.getElementById('btn_0').addEventListener('click', function () {
+    inputWindow.value += '0';
+})
+
+document.getElementById('btn_equally').addEventListener('click', function () {
+    if (operation === 'sum') {
+        const result = lastOperand + parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    } else if (operation === 'subtraction') {
+        const result = lastOperand - parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    } else if (operation === 'multiplication') {
+        const result = lastOperand * parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    } else if (operation === 'division') {
+        const result = lastOperand / parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    }
+})
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_subtraction').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'subtraction';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_multiplication').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'multiplication';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_root').addEventListener('click', function () {
+    lastOperand = Math.sqrt(parseInt(inputWindow.value));
+    const result = lastOperand;
+    lastOperand = 0;
+    inputWindow.value = result;
+})
+
+document.getElementById('btn_division').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'division';
+    inputWindow.value = '';
+})
+
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,4 +1,4 @@
-let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
+/* let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
 let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
@@ -43,4 +43,202 @@ document.getElementById('btnEqual').addEventListener('click', function () {
         gameRun = false;
     }
 })
+ */
+let minValue;
+let maxValue;
+let gameRun = true;
+let orderNumber = 1;
+let answerNumberLetters;
+let answerNumber;
+
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+var alertTrigger = document.getElementById('liveAlertBtn');
+const orderNumberField = document.getElementById('orderNumberField');
+const answerField = document.getElementById('answerField');
+
+function retry() {
+    document.querySelector('#orderNumberField').style.display = 'none';
+    document.querySelector('#cardHeader').style.display = 'none';
+    document.querySelector('#cardBody1').style.display = 'none';
+    document.querySelector('#cardBody2').style.display = 'none';
+    document.querySelector('#answerField').style.display = 'none';
+    document.querySelector('.card-footer').style.display = 'none';
+    runToMinMaxValue();
+}
+
+function translatingNumbers (value) {
+    let firstPlace = ['сто' , 'двести' , 'триста' , 'четыреста', 'пятьстот' ,  'шетьсот' , 'семьсот' , 'восемьсот', 'девятсот'];
+    let secondPlace = ['десять' , 'двадцать' , 'тридцать' , 'сорок', 'пятьдесят' ,  'шетьдесят' , 'семьдесят' , 'восемьдесят', 'девяносто'];
+    let thirdPlace = ['один' , 'два' , 'три' , 'четыре', 'пять' ,  'шеть' , 'семь' , 'восемь', 'девять'];
+    let invalidNumber = ['одинадцать' , 'двенадцать' , 'тринадцать' , 'четырнадцать' , 'пятнадцать' , 'шестнадцать' , 'семнадцать' , 'восемнадцать' , 'девятнадцать'];
+    let returnValue = "";
+    let returnValue1 = "";
+    let returnValue2 = "";
+    let returnValue3 = "";
+
+    var obStr=new String(value);
+
+    let numReversed = obStr.split("");
+
+    console.log(numReversed);
+    if ((answerNumber < 10)&&(answerNumber > 0))  {
+        for (i=0; i < thirdPlace.length; i++) {
+            if (numReversed[0] == (i+1)) {
+                returnValue1 = thirdPlace[i];
+            }
+        }
+    } else if ((answerNumber > 10)&&(answerNumber < 20)) {
+        for (i=0; i < invalidNumber.length; i++) {
+            if (numReversed[1] == (i+1)) {
+                returnValue1 = invalidNumber[i];
+            }
+        }
+    } else if ((answerNumber >= 10)&&(answerNumber < 100)) {
+        for (i=0; i < secondPlace.length; i++) {
+            if (numReversed[0] == (i+1)) {
+                returnValue2 = secondPlace[i];
+            }
+        }
+        for (i=0; i < thirdPlace.length; i++) {
+            if (numReversed[1] == (i+1)) {
+                returnValue1 = thirdPlace[i];
+            }
+        }
+    } else if ((answerNumber > 110)&&(answerNumber < 120)) {
+        for (i=0; i < firstPlace.length; i++) {
+            if (numReversed[0] == (i+1)) {
+                returnValue3 = firstPlace[i];
+            }
+        }
+        for (i=0; i < invalidNumber.length; i++) {
+            if (numReversed[2] == (i+1)) {
+                returnValue1 = invalidNumber[i];
+            }
+        }
+    } else if ((answerNumber >= 100)&&(answerNumber <= 200)) {
+        for (i=0; i < firstPlace.length; i++) {
+            if (numReversed[0] == (i+1)) {
+                returnValue3 = firstPlace[i];
+            }
+        }
+        for (i=0; i < secondPlace.length; i++) {
+            if (numReversed[1] == (i+1)) {
+                returnValue2 = secondPlace[i];
+            }
+        }
+        for (i=0; i < thirdPlace.length; i++) {
+            if (numReversed[2] == (i+1)) {
+                returnValue1 = thirdPlace[i];
+            }
+        }
+    }
+    
+    returnValue = `${returnValue3} ${returnValue2} ${returnValue1}`;
+    console.log(returnValue);
+    return returnValue;
+
+
+}
+
+function runGame() {
+
+    answerNumber  = Math.floor((maxValue - minValue) / 2);
+    let messagesVictory = [`Я всегда угадываю\n\u{1F60E}` , `У меня просто кибермозг\n\u{1F92F}`, `Машины рулят!!!\n\u{1F914}`];
+
+    document.querySelector('#questionsToAnswer').style.display = 'none';
+    document.querySelector('#orderNumberField').style.display = 'block';
+    document.querySelector('#cardHeader').style.display = 'block';
+    document.querySelector('#cardBody1').style.display = 'block';
+    document.querySelector('#cardBody2').style.display = 'block';
+    document.querySelector('#answerField').style.display = 'block';
+    document.querySelector('.card-footer').style.display = 'block';
+
+    orderNumberField.innerText = orderNumber;
+    answerNumberLetters = translatingNumbers(answerNumber);
+    answerField.innerText = `Вы загадали число ${answerNumberLetters}?`;
+
+    document.getElementById('btnLess').addEventListener('click', function () {
+
+        maxValue = answerNumber - 1;
+        answerNumber = (maxValue - Math.floor((maxValue - minValue) / 2)) ;
+        orderNumber++;
+        orderNumberField.innerText = orderNumber;
+
+        answerNumberLetters = translatingNumbers(answerNumber);
+
+        let messagesloss = [`Вы загадали неправильное число!\n\u{1F914}` , `Я сдаюсь..\n\u{1F92F}`, `Как ты обманул меня хитрый человек?!\n\u{1F914}`];
+        let messagesQuestion = [`Вы загадали число ${answerNumberLetters}?` , `А может это ${answerNumberLetters}?`, `По моему число ${answerNumberLetters} загаданное?`];
+        
+        if (maxValue < minValue){
+            const randomIndex = Math.round(Math.random()*(messagesloss.length-1));
+            answerField.innerText = messagesloss[randomIndex];
+        } else {
+            const randomIndex = Math.round(Math.random()*(messagesQuestion.length-1));
+            answerField.innerText = messagesQuestion[randomIndex];
+        }
+        console.log(answerNumber, minValue, maxValue);
+    });
+
+    document.getElementById('btnOver').addEventListener('click', function () {
+
+        minValue = answerNumber + 1;
+        answerNumber = (maxValue - Math.floor((maxValue - minValue) / 2)) ;
+        orderNumber++;
+        orderNumberField.innerText = orderNumber;
+
+        let answerNumberLetters = translatingNumbers(answerNumber);
+
+        let messagesloss = [`Вы загадали неправильное число!\n\u{1F914}` , `Я сдаюсь..\n\u{1F92F}`, `Как ты обманул меня хитрый человек?!\n\u{1F914}`];
+        let messagesQuestion = [`Вы загадали число ${answerNumberLetters }?` , `А может это ${answerNumberLetters}?`, `По моему число ${answerNumberLetters} загаданное?`];
+
+        if (answerNumber > maxValue){
+            const randomIndex = Math.round(Math.random()*(messagesloss.length-1));
+            answerField.innerText = messagesloss[randomIndex];
+        } else {
+            const randomIndex = Math.round(Math.random()*(messagesQuestion.length-1));
+            answerField.innerText = messagesQuestion[randomIndex];
+        }
+        console.log(answerNumber, minValue, maxValue);
+    });
+
+    document.getElementById('btnEqual').addEventListener('click', function () {
+        if (gameRun){
+            const randomIndex = Math.round(Math.random()*(messagesVictory.length-1));
+            answerField.innerText = messagesVictory[randomIndex];
+        };
+    });
+}
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+function validateForm() {
+    minValue = document.getElementById('minValue').value;
+    maxValue = document.getElementById('maxValue').value;
+    if((minValue < maxValue)&&(minValue > 0)&&(maxValue <= 200)) {
+        questionsToAnswer();
+    } else  {
+        alert('Вы не ввели значения диапазона!!!!','danger');
+        minValue.value = 1;
+        maxValue = 200;
+    }
+}
+
+function questionsToAnswer() {
+    document.querySelector('#minMaxValue').style.display = 'none';
+    document.querySelector('#questionsToAnswer').style.display = 'block';
+    let attempt = Math.ceil(Math.log2((maxValue - minValue) + 1));
+    answerField1 = document.getElementById('answerField1');
+    answerField1.innerText = `Число будет угадано за ${attempt} вопроса(ов)!`;
+
+}
+
+function runToMinMaxValue() {
+    document.querySelector('#minMaxValue').style.display = 'block';
+    document.querySelector('#greeting').style.display = 'none';
+}
 
